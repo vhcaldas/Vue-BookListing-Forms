5bb84411ac67ed2f6c896fa4e64b98170df70933
Vivian Caldas <54189003+vhcaldas@users.noreply.github.com>
2020-07-09 14:28:33 -0300

    Refactoring BookForm to integrate with BookData

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,15 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <input type="checkbox" v-model="bookData.finishedReading">
+      <label for="finishedReading">Finished Reading</label>
+      <input type="radio" v-model="bookData.ownership" value="borrowed">
+      <label for="borrowed">Borrowed</label>
+      <input type="radio" v-model="bookData.ownership" value="bought">
+      <label for="bought">Bought</label>
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,13 +20,17 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData:{
+        bookTitle: '',
+        bookAuthor: '',
+        finishedReading: false,
+        ownership: [],
+      }
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
